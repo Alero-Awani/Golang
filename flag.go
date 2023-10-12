506c13d3e45d69f829d2fdfa9d25b6baa5a33a41
Alero Awani <aleroawani18@gmail.com>
2023-10-12 14:37:44 +0100

    added flag.go

---
+++ flag.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+//allow the user specify what text message they want printed on the console by using a flag
+//if the user doesnt specify a flag then we want a default message to be printed
+
+func main(){
+	//set  a flag to specify the message to display
+	//text is the name of the flag, what comes after is the default message , then a help message to show how the flag works 
+	// not that the flag returns a pointer, so msg is a pointer 
+	//recall that if you want to use a pointer you need to dereference it  
+	msg := flag.String("text", "Hello there!","message to display")
+
+	caps := flag.Bool("c", false, "should text be uppercase")
+	flag.Parse()
+
+
+	if *caps {
+		*msg = strings.ToUpper(*msg)
+	}
+	// display message 
+	fmt.Println(*msg)
+
+
+
+
+}
+
+
+//run go run flag.go -text=
